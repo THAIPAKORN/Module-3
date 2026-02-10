41dfabc62d016eb3ce36ed99fdf7296f6a2316ae
SUNNY <sun6710301025@gmail.com>
2026-02-10 04:48:40 +0000

    Lab5

---
+++ wecwebdemo/src/main/java/com/example/wecwebdemo/WecwebdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.wecwebdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WecwebdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WecwebdemoApplication.class, args);
+	}
+
+}
---
+++ wecwebdemo/src/test/java/com/example/wecwebdemo/WecwebdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wecwebdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WecwebdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
